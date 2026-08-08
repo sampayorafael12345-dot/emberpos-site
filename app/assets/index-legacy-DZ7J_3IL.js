9f628dedb47799b6cce236ece92d11182a1acf2a
Rafael Sampayo <sampayorafael12345@gmail.com>
2026-08-08 23:05:40 +0800

    Call the checkout function with fetch so the error body is never lost; send the current branch

---
+++ app/assets/index-legacy-DZ7J_3IL.js
@@ -0,0 +1,1 @@
+System.register(["./index.web-legacy-cy0k8tJ8.js"],function(e,n){"use strict";var t,r;return{setters:[function(e){t=e.r,r=e._}],execute:function(){e("App",t("App",{web:function(){return r(function(){return n.import("./web-legacy-skbn95zX.js")},void 0).then(function(e){return new e.AppWeb})}}))}}});
