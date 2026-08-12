0596113d7f3d6b20440c6c99728a097bb1970e75
Rafael Sampayo <sampayorafael12345@gmail.com>
2026-08-13 03:17:25 +0800

    App v1.1.50: browsable update history in Help and Support

--- app/assets/index-legacy-D7QyHJfz.js
+++
@@ -1,1 +0,0 @@
-System.register(["./index.web-legacy-C8jU4zFa.js"],function(e,n){"use strict";var t,r;return{setters:[function(e){t=e.r,r=e._}],execute:function(){e("App",t("App",{web:function(){return r(function(){return n.import("./web-legacy-DOgXJOhr.js")},void 0).then(function(e){return new e.AppWeb})}}))}}});
