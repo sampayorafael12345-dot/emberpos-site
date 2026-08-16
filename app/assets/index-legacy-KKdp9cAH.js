9b5e017b621738adaa463df976143193d3535e41
Rafael Sampayo <sampayorafael12345@gmail.com>
2026-08-17 03:56:04 +0800

    v1.1.65: shift breaks, Purchase Orders tab, calendar pickers
    Pause a shift for a break while another cashier covers with her own shift
    on the same drawer; purchases get their own tab with received-by and
    inline items; Receive Now asks about drawer payment; waste history;
    calendar date pickers; kitchen prep column.

--- app/assets/index-legacy-KKdp9cAH.js
+++
@@ -1,1 +0,0 @@
-System.register(["./index.web-legacy-xQq4mvZx.js"],function(e,r){"use strict";var n,t;return{setters:[function(e){n=e.r,t=e._}],execute:function(){e("Browser",n("Browser",{web:function(){return t(function(){return r.import("./web-legacy-JkjTzb4O.js")},void 0).then(function(e){return new e.BrowserWeb})}}))}}});
