bf0f9ac61862c9e3845282f27ef50b1591354e19
Rafael Sampayo <sampayorafael12345@gmail.com>
2026-08-23 00:05:01 +0800

    v1.1.80: batch 26 - custody handshakes, takeout packaging, per-size addon counts + quantities, expense void, board overlay, referrals, terms page, menu-import offer, iPhone pass

--- app/assets/index-legacy-DZygJsRl.js
+++
@@ -1,1 +0,0 @@
-System.register(["./index.web-legacy-CbiJxiBV.js"],function(e,r){"use strict";var n,t;return{setters:[function(e){n=e.r,t=e._}],execute:function(){e("Browser",n("Browser",{web:function(){return t(function(){return r.import("./web-legacy-x8VYDe6d.js")},void 0).then(function(e){return new e.BrowserWeb})}}))}}});
