e1793e77b4c677b28d31611a7ab57eb4b263b811
Rafael Sampayo <sampayorafael12345@gmail.com>
2026-08-28 00:57:54 +0800

    The Now Serving switch works, and the console can ping an update
    publicInfo() was reading hide_board_tab and then dropping it from its
    return, so the sidebar could never hide the row on web or Android. The ops
    console gains a button that points every till at a version - refusing any
    version no Android device has actually been seen running - and "Email
    them" copies the address instead of relying on a mail app being installed.

---
+++ app/assets/index-legacy-DQmORq6w.js
@@ -0,0 +1,1 @@
+System.register(["./index.web-legacy-wmTUXFd0.js"],function(e,r){"use strict";var n,t;return{setters:[function(e){n=e.r,t=e._}],execute:function(){e("Browser",n("Browser",{web:function(){return t(function(){return r.import("./web-legacy-0x90LDDg.js")},void 0).then(function(e){return new e.BrowserWeb})}}))}}});
