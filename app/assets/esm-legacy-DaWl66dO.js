b55f3a8b895cfc3540c8517b5f64a3437598dc1a
Ember POS <atlasco.web@gmail.com>
2026-09-25 22:23:24 +0800

    The app: 1.5.0 - hand-over and complete react on the tap, twelve colour themes, Dim and Soft light

---
+++ app/assets/esm-legacy-DaWl66dO.js
@@ -0,0 +1,1 @@
+(function(){System.register([`./index.web-legacy-BDYnzpfy.js`],function(e,t){var n,r;return{setters:[function(e){n=e.n,r=e.r}],execute:function(){e(`Browser`,n(`Browser`,{web:function(){return r(function(){return t.import(`./web-legacy-BnuZdvKV.js`).then(function(e){return new e.BrowserWeb})},void 0)}}))}}})})();
