5725a86a71f4a1477b4f1dd8040be6c72ec06103
Ember POS <atlasco.web@gmail.com>
2026-09-25 21:33:37 +0800

    The app: 1.5.0

--- app/assets/esm-legacy--AVIeG1x.js
+++
@@ -1,1 +0,0 @@
-(function(){System.register([`./index.web-legacy-B0M_o0Ph.js`],function(e,t){var n,r;return{setters:[function(e){n=e.n,r=e.r}],execute:function(){e(`Browser`,n(`Browser`,{web:function(){return r(function(){return t.import(`./web-legacy-BNKjJopg.js`).then(function(e){return new e.BrowserWeb})},void 0)}}))}}})})();
